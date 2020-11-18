f131bec6c86167b2d4fd434f360ab89bcacc5c36
Andrzej Dziekoński <andrzej@sigmeo.pl>
2020-11-18 12:25:41 +0100

    blob fixes

--- Entity/Type/SafeObjectType.php
+++ Entity/Type/SafeObjectType.php
@@ -2,7 +2,10 @@
 
 namespace JMS\JobQueueBundle\Entity\Type;
 
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
 use Doctrine\DBAL\Types\ObjectType;
+use Doctrine\DBAL\Types\Types;
 
 class SafeObjectType extends ObjectType
 {
@@ -11,6 +14,27 @@ class SafeObjectType extends ObjectType
         return $platform->getBlobTypeDeclarationSQL($fieldDeclaration);
     }
 
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        if (is_string($value)) {
+            $fp = fopen('php://temp', 'rb+');
+            assert(is_resource($fp));
+            fwrite($fp, $value);
+            fseek($fp, 0);
+            $value = $fp;
+        }
+
+        if (! is_resource($value)) {
+            throw ConversionException::conversionFailed($value, Types::BLOB);
+        }
+
+        return $value;
+    }
     public function getName()
     {
         return 'jms_job_safe_object';
